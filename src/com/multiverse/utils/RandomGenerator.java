fae2eb250443c45104335472a4cca351aa4e1938
skylarker <arunprasath9586@gmail.com>
2015-07-04 04:12:42 -0700

    build.gradle cleaned | gitignore updated | adding a few packages

---
+++ src/com/multiverse/utils/RandomGenerator.java
@@ -0,0 +1,17 @@
+package com.multiverse.utils;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RandomGenerator {
+    public List<Integer> generateRandomIntegers(int n) {
+        List<Integer> randomIntegers = new ArrayList<>();
+        return randomIntegers;
+    }
+
+    private List<Float> generateRandomFloats(int n) {
+        List<Float> randomFloats = new ArrayList<>();
+        return randomFloats;
+    }
+}
